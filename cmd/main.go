7cd2f2f5474ae1f389ae41a856fcb7c4a5ded76a
serj213 <kornilovsergey55@gmail.com>
2025-02-11 10:59:37 +0300

    added logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/serj213/bookService/internal/config"
+)
+
+const (
+	local = "local"
+	dev = "dev"
 )
 
 func main(){
@@ -13,5 +19,24 @@ func main(){
 		panic(err)
 	}
 
-	fmt.Println("cfg: ", cfg)
+	log := setupLogger(cfg.Env)
+
+	log = log.With(slog.String("service", "bookService"))
+
+	log.Info("logger enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch(env) {
+	case local:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case dev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default: 
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
